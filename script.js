7306f6414dc8a9e34657dacef44a1957238bebf5
Marcus Dev <163601890+MarcusDevinicius@users.noreply.github.com>
2025-01-18 10:59:33 -0300

    Treinando Mais

--- script.js
+++ script.js
@@ -1,63 +1,19 @@
-const linksInternos = document.querySelectorAll('.menu a[href^="#"]');
+const maisInfo = document.querySelectorAll('.mais-info');
 
-if (linksInternos.length) {
-    function initNavMenu() {
-        function navMenu(event) {
-            event.preventDefault();
-            const href = event.currentTarget.getAttribute('href');
-            const section = document.querySelector(href);
-            const sectionTop = section.offsetTop - 90;
-            window.scrollTo({
-                top: sectionTop,
-                behavior: 'smooth',
-            })
-        }
-
-
-
-        linksInternos.forEach((link) => {
-            link.addEventListener('click', navMenu);
-        })
-    }
-}
-
-const maisInfo = document.querySelectorAll('section .mais-info');
-
-
-if(maisInfo.length) {
-    function mostrarMais() {
-        function mostrarInfo(event) {
+function initMostrarMais() {
+    if(maisInfo.length) {
+        function verMais(event) {
             const itemSelec = event.currentTarget;
             const textSelec = itemSelec.nextElementSibling;
             textSelec.classList.toggle('ativo');
-            itemSelec.classList.toggle('ativo')
+            itemSelec.classList.toggle('ativo');
         }
 
+
         maisInfo.forEach((info) => {
-            info.addEventListener('click', mostrarInfo);
+            info.addEventListener('click', verMais);
         })
     }
-}
-
-const opGames = document.querySelectorAll('.lista-games a');
-const imgGames = document.querySelectorAll('.img-games img');
-
-
-if(opGames.length && imgGames.length) {
-    imgGames[0].classList.add('ativo')
-    function mostrarImg(index) {
-        imgGames.forEach((img) => {
-            img.classList.remove('ativo');
-        })
-        imgGames[index].classList.add('ativo');
-    }
-
-
-    opGames.forEach((opcao, index) => {
-        opcao.addEventListener('click', () => {
-            mostrarImg(index)
-        })
-    })
 }
 
 const sections = document.querySelectorAll('section');
@@ -65,8 +21,8 @@ const windowMetade = window.innerHeight * 0.7;
 
 function animaScroll() {
     sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-        if (sectionTop < 0) {
+        const topSec = section.getBoundingClientRect().top - windowMetade;
+        if (topSec < 0) {
             section.classList.add('ativo');
         } else {
             section.classList.remove('ativo');
@@ -74,8 +30,6 @@ function animaScroll() {
     })
 }
 
-window.addEventListener('scroll', animaScroll);
+window.addEventListener('scroll', animaScroll)
 
-
-mostrarMais()
-initNavMenu()
+initMostrarMais()
