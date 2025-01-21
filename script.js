314f45be27f0d6dd8ab460f6e1278cd65e168e5b
Marcus Dev <163601890+MarcusDevinicius@users.noreply.github.com>
2025-01-21 11:09:21 -0300

    Treinando mais

--- script.js
+++ script.js
@@ -19,17 +19,68 @@ function initMostrarMais() {
 const sections = document.querySelectorAll('section');
 const windowMetade = window.innerHeight * 0.7;
 
-function animaScroll() {
-    sections.forEach((section) => {
-        const topSec = section.getBoundingClientRect().top - windowMetade;
-        if (topSec < 0) {
-            section.classList.add('ativo');
-        } else {
-            section.classList.remove('ativo');
+function initAnimaScroll() {
+    if (sections.length) {
+        function animaScroll() {
+            sections.forEach((section) => {
+                const topSec = section.getBoundingClientRect().top - windowMetade;
+                if (topSec < 0) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            })
         }
-    })
+
+        window.addEventListener('scroll', animaScroll);
+    }
 }
 
-window.addEventListener('scroll', animaScroll)
+
+const itemMenu = document.querySelectorAll('.menu a[href^="#"]');
+
+function initLinksInternos() {
+    if(itemMenu.length) {
+        function btnToSection(event) {
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute('href');
+            const section = document.querySelector(href);
+            const sectionTop = section.offsetTop - 90;
+            window.scrollTo({
+                top: sectionTop,
+                behavior: 'smooth',
+            })
+        }
+
+        itemMenu.forEach((item) => {
+            item.addEventListener('click', btnToSection)
+        })
+    }
+}
+
+const opGames = document.querySelectorAll('.lista-games a');
+const imgGames = document.querySelectorAll('.img-games img');
+
+function initNavTabs() {
+    if (opGames.length && imgGames.length) {
+        imgGames[0].classList.add('ativo');
+        function navTabs(index) {
+            imgGames.forEach((img) => {
+                img.classList.remove('ativo');
+            })
+            imgGames[index].classList.add('ativo');
+        }
+
+        opGames.forEach((game, index) => {
+            game.addEventListener('click', () => {
+                navTabs(index);
+            });
+
+        })
+    }
+}
 
 initMostrarMais()
+initAnimaScroll()
+initLinksInternos()
+initNavTabs()
